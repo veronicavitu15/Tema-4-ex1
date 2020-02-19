6e9c3eab08bbe618689f003898cea8d36ba18cfb
Veronica <veronicavitu15@gmail.com>
2020-02-19 21:40:35 +0200

    Add project files.

---
+++ Tema 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_4
+{
+    /* //Exercitiul 1
+     class Animal
+    {
+            public string tipAnimal { get; set; }
+            public string culoare { get; set; }
+            public int varsta { get; set; }
+            
+        public Animal()
+        {
+            tipAnimal = "";
+            culoare = "";
+            varsta = 0;
+        }
+            public Animal(string tipAnimal)
+        {
+            this.tipAnimal = tipAnimal;
+            this.culoare = "";
+            this.varsta = 0; 
+        }
+        public Animal(string tip, string culoare, int varsta)
+        {
+            tipAnimal = tip;
+            this.culoare = culoare;
+            this.varsta = varsta;
+        }
+    }*/
+    //Exercitiul 2
+
+}
